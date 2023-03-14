d3fa0c244530e2cbf170595b39b9bdc2a9d9b1eb
MHLEE12 <coolmh12@gmail.com>
2023-03-14 21:59:52 +0900

    230314_오늘의 문제

---
+++ MH/Solution230314.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class Solution230314 {
+//    https://leetcode.com/problems/squares-of-a-sorted-array/?envType=study-plan&id=algorithm-i
+//    977. Squares of a Sorted Array
+    
+    public int[] sortedSquares(int[] nums) {
+        int[] result = new int[nums.length];
+
+        for(int i = 0; i < nums.length; i++) {
+            result[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(result);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] ex = {-4,-1,0,3,10};
+        int[] result1 = new int[ex.length];
+
+        Solution230314 s = new Solution230314();
+        result1 = s.sortedSquares(ex);
+
+        for(int i = 0; i < ex.length; i++) {
+            System.out.println(i + " : " + result1[i]);
+        }
+    }
+}
